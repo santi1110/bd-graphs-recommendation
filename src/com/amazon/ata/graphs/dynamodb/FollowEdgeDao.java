5e3f6e35d60fb8368c35ba58a4c3b4a2d70d6a7b
Santiago Coria <santiago_coria@hotmail.com>
2024-11-17 00:34:35 -0300

    Graphs

--- src/com/amazon/ata/graphs/dynamodb/FollowEdgeDao.java
+++ src/com/amazon/ata/graphs/dynamodb/FollowEdgeDao.java
@@ -31,7 +31,13 @@ public class FollowEdgeDao {
      * @return A list of all follows for the given user
      */
     public PaginatedQueryList<FollowEdge> getAllFollows(String username) {
-        return null;
+        if (username == null || username.isEmpty()){
+            throw new IllegalArgumentException("username not provided");
+        }
+        DynamoDBQueryExpression<FollowEdge> queryExpression = new DynamoDBQueryExpression<>();
+        FollowEdge parameter = new FollowEdge(username, null);
+        queryExpression.withHashKeyValues(parameter);
+        return this.mapper.query(FollowEdge.class, queryExpression);
     }
 
     /**
@@ -40,7 +46,16 @@ public class FollowEdgeDao {
      * @return A list of all followers for the given user
      */
     public PaginatedQueryList<FollowEdge> getAllFollowers(String username) {
-        return null;
+        if (username == null || username.isEmpty()){
+            throw new IllegalArgumentException("username not provided");
+        }
+        DynamoDBQueryExpression<FollowEdge> queryExpression = new DynamoDBQueryExpression<>();
+        Map <String, AttributeValue> attributeValueMap = new HashMap<>();
+        attributeValueMap.put(":toUsername", new AttributeValue().withS(username));
+
+        queryExpression.withKeyConditionExpression("toUsername = :toUsername")
+                .withExpressionAttributeValues(attributeValueMap);
+        return this.mapper.query(FollowEdge.class, queryExpression);
     }
 
     /**
--- src/com/amazon/ata/graphs/lambda/CreateRecommendationsActivity.java
+++ src/com/amazon/ata/graphs/lambda/CreateRecommendationsActivity.java
@@ -4,6 +4,7 @@ import com.amazon.ata.graphs.dynamodb.FollowEdge;
 import com.amazon.ata.graphs.dynamodb.FollowEdgeDao;
 import com.amazon.ata.graphs.dynamodb.Recommendation;
 import com.amazon.ata.graphs.dynamodb.RecommendationDao;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.amazonaws.services.lambda.runtime.Context;
 
 import java.security.InvalidParameterException;
@@ -24,6 +25,30 @@ public class CreateRecommendationsActivity {
     }
 
     public List<Recommendation> handleRequest(CreateRecommendationsRequest input, Context context) {
-        return null;
+        if (input == null|| input.getUsername() == null){
+            throw new InvalidParameterException("missing input");
+        }
+        List<Recommendation> recommendations = new ArrayList<>();
+        PaginatedQueryList <FollowEdge> followEdgePaginatedQueryList = this.followEdgeDao.getAllFollows(input.getUsername());
+        List<String> follows = followEdgePaginatedQueryList.stream()
+        .map(FollowEdge::getFromUsername)
+                .collect(Collectors.toList());
+
+        for (String username : follows){
+            PaginatedQueryList<FollowEdge> followsFollowEdges = this.followEdgeDao.getAllFollows(username);
+            List<String> followsFollows = followsFollowEdges.stream()
+                    .map(FollowEdge::getFromUsername).collect(Collectors.toList());
+            for(String followsFollow : followsFollows){
+                if (followsFollow != input.getUsername() && !follows.contains(followsFollow)
+                        && !recommendations.contains(followsFollow)){
+                    recommendations.add(new Recommendation(input.getUsername(), followsFollow, "active"));
+                    if (recommendations.size() >= input.getLimit()){
+                        return recommendations;
+                    }
+                }
+            }
+        }
+
+        return recommendations;
     }
 }
